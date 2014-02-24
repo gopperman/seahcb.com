273fde2997dcf95096a18e061f3d61c439b53898
Greg Opperman <gopperman@gmail.com>
2014-02-23 20:52:16 -0800

    Adding hover state to header. Closes #3

--- such.js
+++ such.js
@@ -1,14 +1,22 @@
 jQuery('document').ready(function($) {
-	$('h1').hover(
-		function() {
-			console.log('in');
-		},
-		function() {
-			console.log('out');
-		}
-	);
 	$('.dogetoggle').delay(3000).animate({right:"-40px"}, 1000);
 	$('.dogetoggle, .boringtoggle').click(function() {
 		$('body').toggleClass('boring doge');
+		if ($('body').hasClass('doge')) {
+			$('.doge header').hover(
+				function() {
+					$('header .wow').each(function(index) {
+						$(this).delay(600*index).fadeIn(200);
+					});
+				},
+				function() {
+					$('header .wow').each(function(index) {
+						$(this).delay(100*index).fadeOut(100);
+					});
+				}
+			);
+		} else {
+			$('.boring header').unbind('mouseenter mouseleave');
+		}
 	});
 });
